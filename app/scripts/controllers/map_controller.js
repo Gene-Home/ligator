68cf326008f9eb17aaa4bee872f9c707118c920a
ghdulrich <ghdulrich@gmail.com>
2014-10-15 19:14:52 -0400

    added missing map related files

---
+++ app/scripts/controllers/map_controller.js
@@ -0,0 +1,41 @@
+
+angular.module('ligatorApp')
+.controller('MapCtrl', function($scope,EntityService,GeoCoder) {
+
+	//will be set in mapsInitialized
+	$scope.map;
+	$scope.markers = [];
+  	$scope.$on('mapsInitialized', function(event, maps) {
+   	  	$scope.map = maps[0]
+   		GeoCoder.geocode({address: 'Boston,MA'}).then(function(result) {
+   			$scope.map.setCenter(result[0].geometry.location)
+  		});
+    $scope.markAddress = function(org){
+      var addy = org.street + "," + org.city + "," +  org.state + "," +  org.zipcode; 
+      GeoCoder.geocode({address:addy}).then(function(result) {
+        
+        $scope.markers[0] = new google.maps.Marker({title:"YES"});
+        $scope.markers[0].setPosition(result[0].geometry.location);
+        $scope.markers[0].setTitle(org.name);
+        $scope.markers[0].setMap($scope.map);
+
+
+      });
+    }
+   	$scope.testo = function(){
+   		EntityService.all({class:'organization-profile'}).$promise.then(
+        function(success){
+          $scope.orgs = success;
+          for(var idx in success){
+            $scope.markAddress(success[idx]);
+          }    
+        },
+        function(failure){
+          alert("BAD PROJECTS " + failure)
+        }
+        )
+   		
+   		
+   	}
+  });
+});
